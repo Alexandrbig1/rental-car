6974db4ebfe094d4c4b7c0d225b38d7eebc8936f
Alexandrbig1 <alexsmagin1@gmail.com>
2024-02-24 16:06:05 -0600

    Updates

--- assets/Favorites-BBGhYyy2.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-yVNF8SMV.js";function e(){return r.jsx("div",{children:"Favorites"})}export{e as default};
--- assets/Privacy-BMuJGSPS.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-yVNF8SMV.js";import{P as a,a as i}from"./Privacy.styled-CJBGX_77.js";import"./heroBg-xqvqUgNj.js";function e(){return r.jsx(a,{children:r.jsx(i,{children:r.jsx("span",{children:"Privacy"})})})}export{e as default};
