5314cf911f65ebb1773fc24502caea12e90a356a
Alexandrbig1 <alexsmagin1@gmail.com>
2024-02-24 22:54:37 -0600

    Updates

--- assets/Favorites-zjqGxxB-.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-Bdi-Om--.js";function e(){return r.jsx("div",{children:"Favorites"})}export{e as default};
---
+++ assets/NotFound-VJZT-3iD.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B6r88uSO.js";function n(){return o.jsx("div",{children:"404 Oops! Page Not Found!"})}export{n as default};
