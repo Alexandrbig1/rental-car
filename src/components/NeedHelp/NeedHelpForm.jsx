d876b312c0c80bfc76fc3c597793685c2aeb4cbf
Alexandrbig1 <alexsmagin1@gmail.com>
2024-03-05 14:06:11 -0600

    feat: added emailjs package and function for notification from user on need help modal

--- src/components/NeedHelp/NeedHelpForm.jsx
+++ src/components/NeedHelp/NeedHelpForm.jsx
@@ -1,7 +1,9 @@
+import { useRef } from "react";
 import { toast } from "react-toastify";
 import { fetchEmailDB } from "../../services/emailPost";
 import emailRegex from "../../regex/emailRegex";
 import { commonToastOptions } from "../../helpers/toastOptions";
+import emailjs from "@emailjs/browser";
 import {
   NeedHelpFormWrapper,
   NeedHelpInput,
@@ -10,10 +12,14 @@ import {
   NeedHelpTitle,
   NeedHelpCloseModal,
 } from "./NeedHelpForm.styled";
-import { sendEmail } from "../../services/sendEmail";
+
+const { VITE_EMAIL_ID, VITE_EMAIL_TEMPLATE_ID, VITE_EMAIL_API_KEY } =
+  import.meta.env;
 
 // eslint-disable-next-line react/prop-types
 function NeedHelpForm({ handleNeedHelpCloseModal, setNeedHelp }) {
+  const form = useRef();
+
   const handleNeedHelp = async (e) => {
     e.preventDefault();
 
@@ -25,50 +31,52 @@ function NeedHelpForm({ handleNeedHelpCloseModal, setNeedHelp }) {
     const validEmail = emailRegex.test(data.email);
     const validMessage = data.message.length > 0;
 
-    if (validEmail && validMessage) {
-      sendEmail(data);
-      try {
-        await fetchEmailDB(data);
-        toast.success(
-          "Thank you for reaching out! Your message has been received.",
-          commonToastOptions
-        );
-      } catch (error) {
-        toast.error(
-          "Error submitting request. Please try again later.",
-          commonToastOptions
-        );
-      }
-    } else {
+    if (!(validEmail && validMessage)) {
       if (!validMessage && data.email.length === 0) {
         toast.warning(
           "Please fill in all required fields!",
           commonToastOptions
         );
-        return;
-      }
-
-      if (!validEmail) {
+      } else if (!validEmail) {
         toast.warning(
           "Please enter a valid email address!",
           commonToastOptions
         );
-        return;
-      }
-
-      if (!validMessage) {
+      } else {
         toast.warning("Please enter a message!", commonToastOptions);
-        return;
       }
+      return;
     }
 
-    setNeedHelp((prevState) => !prevState);
+    try {
+      await fetchEmailDB(data);
+      await emailjs.sendForm(
+        VITE_EMAIL_ID,
+        VITE_EMAIL_TEMPLATE_ID,
+        form.current,
+        {
+          publicKey: VITE_EMAIL_API_KEY,
+        }
+      );
+
+      toast.success(
+        "Thank you for reaching out! Your message has been received.",
+        commonToastOptions
+      );
+
+      setNeedHelp((prevState) => !prevState);
+    } catch (error) {
+      toast.error(
+        "Error submitting request. Please try again later.",
+        commonToastOptions
+      );
+    }
   };
 
   return (
     <>
       <NeedHelpCloseModal onClick={handleNeedHelpCloseModal} />
-      <NeedHelpFormWrapper onSubmit={handleNeedHelp}>
+      <NeedHelpFormWrapper ref={form} onSubmit={handleNeedHelp}>
         <NeedHelpTitle>NeedHelp</NeedHelpTitle>
         <NeedHelpInput name="email" type="text" placeholder="Email address" />
         <NeedHelpText
